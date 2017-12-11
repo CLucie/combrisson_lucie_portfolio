02749976c6df1ea614f40aeb56ea25449db315f2
CLucie <lucie.combrisson@laposte.net>
2017-12-11 11:06:23 +0100

    Ajout des fichiers Laravel

---
+++ portfolio/app/Http/Controllers/FrontHomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontHomeController extends Controller
+{
+    public function index(){
+    	return view('welcome');
+    }
+}
